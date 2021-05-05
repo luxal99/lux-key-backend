02110d9fd32479e817164bbd5b851954dbad438f
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-05 16:45:35 +0200

    Fix bugs

--- src/car-brand/car-brand.controller.ts
+++ src/car-brand/car-brand.controller.ts
@@ -1,7 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { CarBrandService } from './car-brand.service';
+import { GenericController } from '../generic/generic.controller';
+import { CarBrand } from './CarBrand';
 
 @Controller('car-brand')
-export class CarBrandController {
-  constructor(private readonly carBrandService: CarBrandService) {}
+export class CarBrandController extends GenericController<CarBrand> {
+
+  constructor(private readonly service: CarBrandService) {
+    super(service);
+  }
 }
--- src/car-model/car-model.controller.ts
+++ src/car-model/car-model.controller.ts
@@ -1,7 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { CarModelService } from './car-model.service';
+import { GenericController } from '../generic/generic.controller';
+import { CarModel } from './CarModel';
 
 @Controller('car-model')
-export class CarModelController {
-  constructor(private readonly carModelService: CarModelService) {}
+export class CarModelController extends GenericController<CarModel> {
+  constructor(private readonly service: CarModelService) {
+    super(service);
+  }
 }
