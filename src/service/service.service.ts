517c620832a25c27bdb1fd13ced00ec3c405d46c
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-14 13:42:16 +0200

    Service overview

--- src/service/service.service.ts
+++ src/service/service.service.ts
@@ -4,9 +4,11 @@ import { Service } from './Service';
 import { ServiceRepository } from './service.repository';
 
 @Injectable()
-export class ServiceService extends GenericService<Service>{
+export class ServiceService extends GenericService<Service> {
 
   constructor(genericRepository: ServiceRepository) {
-    super(genericRepository, ['serviceKeys','idClient','serviceKeys.idKey','serviceKeys.idKey.idCurrentPrice']);
+    super(genericRepository, ['serviceKeys', 'idClient', 'serviceKeys.idKey',
+      'serviceKeys.idKey.idCurrentPrice', 'serviceKeys.idKey.idCarModel',
+      'serviceKeys.idKey.idCarModel.idCarBrand']);
   }
 }
