825ab706af4edfbe8c641ec3bb117e0d43b05a08
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-09-28 15:12:49 +0200

    Delete report after delete entity

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { GenericService } from '../generic/generic.service';
 import { Report } from './Report';
 import { ReportRepository } from './report.repository';
+import * as fs from 'fs';
 
 @Injectable()
-export class ReportService extends GenericService<Report>{
+export class ReportService extends GenericService<Report> {
 
-  constructor(repository:ReportRepository) {
+  constructor(private repository: ReportRepository) {
     super(repository, []);
+  }
+
+  async delete(id: number): Promise<void> {
+    const reportById: Report = await this.findOne(id);
+    this.repository.delete(id).then(() => {
+      fs.unlinkSync(reportById.path);
+    });
   }
 }
