e2e67955b51316dd0b26e75f25f0966031d2a69b
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-11-02 08:46:25 +0100

    Fix bugs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+
+const Instagram = require('instagram-web-api');
 
 @Controller()
 export class AppController {
+
   constructor(private readonly appService: AppService) {
   }
+
 
 }
