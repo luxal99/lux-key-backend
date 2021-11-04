c66ba2247ec2db7ea3ef1bc822a2e3f48bcfd18c
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-11-04 14:24:44 +0100

    Create environment

--- src/constant/const.ts
+++ src/constant/const.ts
@@ -1,17 +1,18 @@
-import { CarBrand } from '../car-brand/CarBrand';
-import { Key } from '../key/Key';
-import { KeyCategory } from '../key-category/KeyCategory';
-import { KeySubCategory } from '../key-sub-category/KeySubCategory';
-import { KeyPrice } from '../key-price/KeyPrice';
-import { Service } from '../service/Service';
-import { ServiceKey } from '../service-key/ServiceKey';
-import { User } from '../user/User';
-import { Client } from '../client/Client';
-import { Report } from '../report/Report';
-import { KeyBrand } from '../key-brand/KeyBrand';
-import { WorkService } from '../work-service/WorkService';
-import { Message } from '../message/Message';
-import { KeyImage } from '../key-image/KeyImage';
+import { CarBrand } from "../car-brand/CarBrand";
+import { Key } from "../key/Key";
+import { KeyCategory } from "../key-category/KeyCategory";
+import { KeySubCategory } from "../key-sub-category/KeySubCategory";
+import { KeyPrice } from "../key-price/KeyPrice";
+import { Service } from "../service/Service";
+import { ServiceKey } from "../service-key/ServiceKey";
+import { User } from "../user/User";
+import { Client } from "../client/Client";
+import { Report } from "../report/Report";
+import { KeyBrand } from "../key-brand/KeyBrand";
+import { WorkService } from "../work-service/WorkService";
+import { Message } from "../message/Message";
+import { KeyImage } from "../key-image/KeyImage";
+import { ConfigModule } from "@nestjs/config";
 
 export const LIST_OF_ENTITIES = [
   CarBrand,
@@ -20,17 +21,21 @@ export const LIST_OF_ENTITIES = [
   Service, ServiceKey, User, Report,
   KeyBrand, WorkService, Message, KeyImage,
 ];
-export const PASSWORD_REGEX = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$';
-export const TOKEN_NAME = 'Authorization';
+export const PASSWORD_REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$";
+export const TOKEN_NAME = "Authorization";
 
 export class JWTMessage {
-  static ACCESS_DENIED_MESSAGE = 'Pristup odbijen';
-  static INVALID_TOKEN_MESSAGE = 'Nevalidan token';
+  static ACCESS_DENIED_MESSAGE = "Pristup odbijen";
+  static INVALID_TOKEN_MESSAGE = "Nevalidan token";
 }
 
 export class RestRoutes {
-  static PREFIX = '/lux/';
-  static JWT = '/jwt';
+  static PREFIX = "/lux/";
+  static JWT = "/jwt";
 }
 
-export const REPORT_PATH = '/home/luxal/PC/Project/LuxKey/lux-key-frontend/src/assets/reports/';
+export const REPORT_PATH = "/home/luxal/PC/Project/LuxKey/lux-key-frontend/src/assets/reports/";
+export const CONFIG = ConfigModule.forRoot({
+  isGlobal: true,
+  envFilePath: "src/environments/.development.env"
+});
