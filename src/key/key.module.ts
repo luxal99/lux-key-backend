9e15d8047d2d5cffbf2f652054c8ad9865a61a4b
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-11-30 19:51:48 +0100

    Generate built-in report

--- src/key/key.module.ts
+++ src/key/key.module.ts
@@ -1,17 +1,25 @@
-import { Module } from "@nestjs/common";
-import { KeyService } from "./key.service";
-import { KeyController } from "./key.controller";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { KeyRepository } from "./key.repository";
-import { ReportRepository } from "../report/report.repository";
-import { ReportService } from "../report/report.service";
-import { KeyImageService } from "../key-image/key-image.service";
-import { KeyImageRepository } from "../key-image/key-image.repository";
+import { Module } from '@nestjs/common';
+import { KeyService } from './key.service';
+import { KeyController } from './key.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { KeyRepository } from './key.repository';
+import { ReportRepository } from '../report/report.repository';
+import { ReportService } from '../report/report.service';
+import { KeyImageService } from '../key-image/key-image.service';
+import { KeyImageRepository } from '../key-image/key-image.repository';
+import { ServiceRepository } from 'src/service/service.repository';
+import { ServiceService } from 'src/service/service.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([KeyRepository, KeyImageRepository, ReportRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      KeyRepository,
+      KeyImageRepository,
+      ReportRepository,
+      ServiceRepository,
+    ]),
+  ],
   controllers: [KeyController],
-  providers: [KeyService, ReportService, KeyImageService],
+  providers: [KeyService, ReportService, KeyImageService, ServiceService],
 })
-export class KeyModule {
-}
+export class KeyModule {}
